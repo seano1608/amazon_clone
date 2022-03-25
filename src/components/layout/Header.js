b324374bd625629c9f2106f87c382ac22572c737
Sean Thomson <seantomo1680@gmail.com>
2022-03-25 15:34:41 +0200

    material icons added

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,16 +5,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <header className='header'>
-        <nav>
-            <ul>
-                <li>
-                    <NavLink activeClassName='active' to="/home">Home</NavLink>                    
-                </li>
-                <li>
-                <NavLink activeClassName='active' to="/products">Products</NavLink>
-                </li>
-            </ul>
-        </nav>
+        
     </header>
   )
 }
