aa12e6432ffda5f9f7a6521d9b5590ab65c5510d
Sean Thomson <seantomo1680@gmail.com>
2022-03-25 16:03:34 +0200

    all

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,37 +1,42 @@
-import React from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import SearchIcon from '@mui/icons-material/Search';
-import ShoppingBasketIcon from '@mui/icons-material'
+import React from "react";
+import { NavLink } from "react-router-dom";
 import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
 
 const Header = () => {
   return (
-    <header className='header'>
-        <img src='' alt='amazon_logo'/>
-        <div className='header_search'>
-            <input className='header_input' type="text"/>
-            <SearchIcon className='search_icon'/>
+    <header className="header">
+      <img
+        src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
+        alt="Amazon logo"
+      />
+      <div className="header_search">
+        <input className="header_input" type="text" />
+        <SearchIcon className="search_icon" />
+      </div>
+      <div className="header_nav">
+        <div className="header_option">
+          <span className="header_optionOne">Hello Guest</span>
+          <span className="header_optionTwo">Sign In</span>
         </div>
-        <div className="header_nav">
-            <div className="header_option">
-                <span className='header_optionOne'>Hello Guest</span>
-                <span className='header_optionTwo'>Sign In</span>                
-            </div>
-            <div className="header_option">
-                <span className='header_optionOne'>Returns</span>
-                <span className='header_optionTwo'>Orders</span>                
-            </div>
-            <div className="header_option">
-                <span className='header_optionOne'>Your</span>
-                <span className='header_optionTwo'>Prime</span>                
-            </div>
-            <div className="header_optionBasket">
-                <ShoppingBasketIcon className="basket_icon"/>
-                <span className='header_optionTwo header_basketCount'>0</span>
-            </div>
-        </div>
-    </header>
-  )
-}
 
-export default Header
+        <div className="header_option">
+          <span className="header_optionOne">Returns</span>
+          <span className="header_optionTwo">Orders</span>
+        </div>
+
+        <div className="header_option">
+          <span className="header_optionOne">Your</span>
+          <span className="header_optionTwo">Prime</span>
+        </div>
+        <div className="header_optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header_optionTwo header_basketCount">0</span>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
