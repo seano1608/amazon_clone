eb52e98b41d224325ecfea03b5c5e37b4f35b628
Sean Thomson <seantomo1680@gmail.com>
2022-03-30 16:24:53 +0200

    sign-in useState form validation

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,21 +1,43 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./SignIn.css";
 
-
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formIsValid, setFormIsValid] = useState(false);
 
-  const emailRef = useRef();
-  const passRef = useRef();
+  useEffect(() => {
+    console.log("effect running");
+  }, []);
 
+  useEffect(() => {
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity");
+      setFormIsValid(email.includes("@") && password.trim().length > 6);
+    }, 500);
 
-  const signIn = (e) =>{
-    e.preventDefault();
-    const enteredEmail = emailRef.current.value;
-    const enteredPass = passRef.current.value;
-    console.log(enteredEmail);
-    console.log(enteredPass);
+    return () => {
+      console.log("cleanup function before next use effect");
+      clearTimeout(identifier);
+    };
+  }, [email, password]);
+
+  const emailChangeHandler = (e) => {
+    setEmail(e.target.value);
   }
+
+  const passwordChangeHandler = (e) => {
+    setPassword(e.target.value);
+  }
+
+  //Sign-in/Login form details
+  const Login = (e) => {
+    e.preventDefault();
+    console.log(formIsValid);
+  };
+
+
   return (
     <div className="login">
       <Link to="/">
@@ -29,10 +51,10 @@ const SignIn = () => {
         <h1>Sign-In</h1>
         <form>
           <h5>Email</h5>
-          <input type="text" ref={emailRef}/>
+          <input type="text" value={email} onChange={emailChangeHandler} />
           <h5>Password</h5>
-          <input type="password" ref={passRef}/>
-          <button type="submit" className="signIn_button" onClick={signIn}>
+          <input type="password" value={password} onChange={passwordChangeHandler} />
+          <button type="submit" className="signIn_button" onClick={Login}>
             Sign In
           </button>
         </form>
@@ -41,7 +63,9 @@ const SignIn = () => {
           Sale. Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
-        <button className="signIn_registerButton">Create your Amazon Account</button>
+        <button className="signIn_registerButton">
+          Create your Amazon Account
+        </button>
       </div>
     </div>
   );
